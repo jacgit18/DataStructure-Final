a72f0cf13218134f2ec1f3112bf763bfed5ade18
jac <joshuacarpentier21@gmail.com>
2019-12-15 18:06:13 -0500

    work on 7, 3, 5, 9, 10

--- 98.FinalExam-Setup/src/main/java/edu/citytech/cst/finalexam/_questions/F10.java
+++ 98.FinalExam-Setup/src/main/java/edu/citytech/cst/finalexam/_questions/F10.java
@@ -3,6 +3,7 @@ package edu.citytech.cst.finalexam._questions;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 import edu.citytech.cst.finalexam.dao.StockDAO;
 import edu.citytech.cst.finalexam.model.Stock;
@@ -14,6 +15,8 @@ public class F10 {
 	 * */
 	 public static void main(String[] args) {		 
 		 List<Stock> list = new StockDAO().findAll();
+		 Predicate<Stock> SymEx = e -> e.getPrice() > 0;
+
 		 long count = list.stream()
 		     .filter(e -> e.getDividendFrequence().equals("Quarterly") )
 		     .map( e -> {
