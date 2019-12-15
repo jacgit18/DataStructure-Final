0075cffc153a20bc6b1f142b8a7d31158afcc1c4
jac <joshuacarpentier21@gmail.com>
2019-12-15 17:27:08 -0500

    add

--- 98.FinalExam-Setup/src/main/java/edu/citytech/cst/finalexam/_questions/F03.java
+++ 98.FinalExam-Setup/src/main/java/edu/citytech/cst/finalexam/_questions/F03.java
@@ -18,11 +18,11 @@ public class F03 {
 	 public static void main(String[] args) {		 
 		 List<Stock> list = new StockDAO().findAll();
 		Predicate<Stock> SymEx = e -> e.toString().contains(e.getSymbol());
-//		Predicate<Stock> SymEx1 = e -> e.getSymbol().compareTo("I")<0;
+		Predicate<Stock> SymEx1 = e -> e.getSymbol().contentEquals("");
 
 
 		 long count = list.stream()
-		     .filter(e -> e.getSymbol().compareTo("I") < 0)
+		     .filter(e -> e.getSymbol().compareTo("I") < 0).filter(SymEx1)
 		     .map( e -> {
 		    	 Map<String,String> map = new HashMap<>();
 		    	 map.put("symbol", e.getSymbol());
@@ -32,14 +32,15 @@ public class F03 {
 		     .peek(System.out::println)
 		     .count();
 		 
-		 
+		 System.out.println("count: " + count);
+
 //		 list.stream().filter(SymEx)
 //			.forEach(System.out::println);
-//			
-//			
+			
+		 // w t v o s m n j u x p
+			
 //			long size = list.stream().filter(SymEx).toArray().length;
 //			System.out.println("Number of Stocks: " + size);
 		 
-		 System.out.println("count: " + count);
 	 }
 }
